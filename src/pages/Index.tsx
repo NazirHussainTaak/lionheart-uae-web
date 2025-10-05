07caa809ef79cdeee43b5822814915348c447914
NazirHussainTaak <nazirhussaintaak@gmail.com>
2025-10-05 23:25:38 +0400

    value pillar

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,15 @@
 //import HeroSection from "@/components/home/HeroSection";
-import HeroSection from "@/pages/home/01-HeroSection";
-import ValuePillarsSection from "@/components/home/ValuePillarsSection";
-import TechnologiesSection from "@/components/home/TechnologiesSection";
-import LeadershipSection from "@/components/home/LeadershipSection";
+import HeroSection from "@/pages/home/01-HeroSection";import HeroSection from "@/pages/home/01-HeroSection";
+import ValuePillarsSection from "@/pages/home/02-ValuePillarsSection";
+import AboutUsSection from "@/pages/home/03-AboutUsSection";
+import SolutionsSection from "@/pages/home/04-SolutionsSection";
+import TechnologiesSection from "@/pages/home/05-TechnologiesSection";
+import TestimonialsSection from "@/pages/home/06-TestimonialsSection";
+import LeadershipSection from "@/pages/home/07-LeadershipSection";
+
+// import ValuePillarsSection from "@/components/home/ValuePillarsSection";
+// import TechnologiesSection from "@/components/home/TechnologiesSection";
+// import LeadershipSection from "@/components/home/LeadershipSection";
 // import AnimatedSection from "@/components/AnimatedSection";
 // import SEOHead from "@/components/SEOHead";
 
@@ -11,7 +18,14 @@ const Index = () => {
     <>
       {/* <SEOHead /> */}
       <div className="min-h-screen">
-        <HeroSection />
+
+        <AnimatedSection animation="fade-slide"> 
+          <HeroSection />
+        </AnimatedSection> 
+
+        <AnimatedSection animation="fade-slide"> 
+          <ValuePillarsSection />
+        </AnimatedSection> 
         {/* <AnimatedSection animation="fade-slide"> */}
         {/*<ValuePillarsSection />*/}
         {/* </AnimatedSection> */}
