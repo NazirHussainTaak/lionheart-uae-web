f3c0417ada99421a4cfadfb12c27e7217f2612a1
NazirHussainTaak <nazirhussaintaak@gmail.com>
2025-10-07 11:05:24 +0400

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-// src/main.tsx (or wherever you mount the router)
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { HashRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootEl = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
     <HashRouter>
       <App />
-    </React.StrictMode>
+    </HashRouter>
   </React.StrictMode>
 );
 
