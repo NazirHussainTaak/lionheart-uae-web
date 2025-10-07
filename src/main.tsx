fb845fd9e58e3886ec3cc10c1f136b21ee10943f
NazirHussainTaak <nazirhussaintaak@gmail.com>
2025-10-07 10:46:57 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,21 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import "./i18n/config";
+// src/main.tsx (or wherever you mount the router)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </React.StrictMode>
+  </React.StrictMode>
+);
+
+
+// import { createRoot } from "react-dom/client";
+// import App from "./App.tsx";
+// import "./index.css";
+// import "./i18n/config";
+
+// createRoot(document.getElementById("root")!).render(<App />);
