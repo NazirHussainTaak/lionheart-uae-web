256da69eca2a24c135adbf44b54ea4abfdd0a389
NazirHussainTaak <nazirhussaintaak@gmail.com>
2025-10-07 11:48:41 +0400

    rollback

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,6 @@
-// main.tsx
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n/config";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
-    <App />
-  </BrowserRouter>
-);
-
-
-// import { createRoot } from "react-dom/client";
-// import App from "./App.tsx";
-// import "./index.css";
-// import "./i18n/config";
-
-// createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);
